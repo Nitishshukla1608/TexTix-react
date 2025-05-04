3398a32f5ad9517cbb3a33c82c3517b15696debf
Nitishshukla1608 <shuklanitish057@gmail.com>
2025-05-04 11:59:17 +0530

    adding more features

--- src/App.js
+++ src/App.js
@@ -1,47 +1,68 @@
+import "./App.css";
+import Navbar from "./Components/Navbar";
+import TextForm from "./Components/TextForm";
+import About from "./Components/About";
+import Alert from "./Components/Alert";
+import { useState } from "react";
+import {
+  BrowserRouter as Router, 
+  Routes,
+ Route ,
+Link
+} from 'react-router-dom';
 
-import './App.css';
-import Navbar from './Components/Navbar';
-import TextForm from './Components/TextForm';
-import About from './Components/About';
-import Alert from './Components/Alert';
-import { useState } from 'react';
+
 
 function App() {
-const [mode, setMode] = useState("light") // whether dark mode is enabled or not 
-const [alert, setAlert] = useState(null)
+  const [mode, setMode] = useState("light"); // whether dark mode is enabled or not
+  const [alert, setAlert] = useState(null);
 
-const showAlert= (message,type)=>{
-  setAlert({
-    msg:message,
-    type:type
-  })
-  setTimeout(() => {
-    setAlert(null)
-  }, 1500);
-}
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-const toggleMode=()=>{
-  if(mode==="light"){
-    setMode("dark")
-    document.body.style.backgroundColor="#042743"
-    showAlert("Dark mode has beed enabled","success");
-  }
-  else{
-    setMode("light")
-    document.body.style.backgroundColor="white"
-    showAlert("Light mode has beed enabled","success");
-  }
-}
+  const toggleMode = () => {
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "#042743";
+      showAlert("Dark mode has beed enabled", "success");
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white";
+      showAlert("Light mode has beed enabled", "success");
+    }
+  };
   return (
-  <>
- {/* ham yahan pe navbar ko import kar rahe hain components se  */}
-<Navbar title="TextTix"  aboutUs="About" mode={mode} toggleMode={toggleMode}/>
-<Alert alert={alert}/>
-<div className="container my-3">
-  <TextForm showAlert={showAlert}heading="Enter the text to convert" mode={mode}/>
-  {/* <About/> */}
-  </div>
-</>
+    <Router>
+      <Navbar
+        title="TextTix"
+        aboutUs="About"
+        mode={mode}
+        toggleMode={toggleMode}
+      />
+      <div className="container my-3">
+        {/* <Alert alert={alert} /> */}
+        <Routes>
+          <Route path="/about" element={<About />} />
+          <Route
+            path="/textform"
+            element={
+              <TextForm
+                showAlert={showAlert}
+                heading="Enter the text to convert"
+                mode={mode}
+              />
+            }
+          />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import PropTypes from 'prop-types'
- 
+import { Link } from 'react-router-dom';
 
 export default function Navbar(props) {
   return (<>    <nav
@@ -8,17 +8,17 @@ export default function Navbar(props) {
     data-bs-theme={props.mode}
   >
   <div className="container-fluid">
-    <a className="navbar-brand" href="#"><b>{props.title}</b></a>
+    <Link className="navbar-brand" to="/textform"><b>{props.title}</b></Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <Link className="nav-link active" aria-current="page" to="/textform">Home</Link>
         </li>
         <li className="nav-item">
-          <a className="nav-link" href="#">{props.aboutUs}</a>
+          <Link className="nav-link" to="/about">{props.aboutUs}</Link>
         </li>
         
       </ul>
